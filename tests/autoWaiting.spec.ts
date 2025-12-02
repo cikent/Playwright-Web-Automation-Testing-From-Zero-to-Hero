7e63e0710ac3dce3bb631853f8097f9e06380ca5
cikent <christopherikent@gmail.com>
2025-12-02 06:59:52 -0800

    Section 4, Lesson 31 Continued
    Signed-off-by: cikent <christopherikent@gmail.com>

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -8,5 +8,25 @@ test.beforeEach(async ({ page }) => {
 test('Auto Waiting', async ({ page }) => {
     const successButton = page.locator('.bg-success')
 
-    await successButton.click()
+    //await successButton.click()
+
+    //const text = await successButton.textContent()
+    // await successButton.waitFor({ state: "attached" })
+    // const text = await successButton.allTextContents()
+
+    // expect(text).toContain('Data loaded with AJAX get request.')
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+})
+
+test('Alternative Waits', async ({ page }) => {
+    const successButton = page.locator('.bg-success')
+
+    //___ wait for element
+    await page.waitForSelector('.bg-success')
+
+    //___ wait for particular response
+    
+    const text = await successButton.allTextContents()
+    expect(text).toContain('Data loaded with AJAX get request.')
 })
