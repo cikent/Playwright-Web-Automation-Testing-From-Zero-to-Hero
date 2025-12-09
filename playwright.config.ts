3caa8d40fdbf695770ecd037696d8d50b10ad96d
cikent <christopherikent@gmail.com>
2025-12-09 06:58:35 -0800

    Section 4, Lesson 32 Started
    Signed-off-by: cikent <christopherikent@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,15 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // timeout: 10000,
+  use: {
+    // globalTimeout: 300000,       // Global timeout for the whole test suite; no default
+    // timeout: 10000,              // Test timeout for each test; default is 30 seconds / 30000 ms
+    // actionTimeout: 0,            // Maximum time each action, Ex. click(), fill(), textContent(), etc.; default is 0
+    // navigationTimeout: 30000,    // Maximum time for navigation methods; default is 0 seconds
+  },
+  expect: {
+    timeout: 5000,                  // Maximum time expect() should wait for the condition to be met; default is 5000 ms
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
