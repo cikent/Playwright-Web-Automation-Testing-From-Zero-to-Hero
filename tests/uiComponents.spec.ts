73cf2bb1373d5d2266566f71ef1fe011bd16e496
cikent <christopherikent@gmail.com>
2026-01-17 04:14:12 -0800

    Section 5, Lesson 35 Checkboxes Complete
    Signed-off-by: cikent <christopherikent@gmail.com>

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -38,6 +38,23 @@ test.describe('Form Layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()).toBeFalsy()     //1st way of assertion, obtaining the Status value and asserting after checking Option 2
         expect(await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).isChecked()).toBeTruthy()        //2nd way of assertion, directly asserting on the Locator after checking Option 2
     })
+})
 
+test('Checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()                                        //Navigate to the Modal & Overlays page via the Sidebar
+    await page.getByText('Toastr').click()                                                  //Select Toastr from the Modal & Overlays submenu 
+
+    await page.getByRole('checkbox', { name: 'Hide on click'}).click({force: true})         //Click the checkbox Element with the name value; enable force: true because element is not visible/interactive
+    await page.getByRole('checkbox', { name: 'Hide on click' }).check({ force: true })      //Check the checkbox Element with the name values Status; enable force: true since element is not visible/interactive
+    await page.getByRole('checkbox', { name: 'Hide on click' }).uncheck({ force: true })      //Uncheck the checkbox Element with the name values Status; enable force: true since element is not visible/interactive
+    await page.getByRole('checkbox', { name: 'Prevent arising of duplicate toast' }).check({ force: true })      //Check the checkbox Element with the name values Status; enable force: true since element is not visible/interactive
+
+    const allBoxes = page.getByRole('checkbox')                             //Get all checkbox elements on the page      
+    for(const box of await allBoxes.all()){                                 //Use JSON syntax to iterate through each checkbox element
+        await box.check({force: true})                                      //Check each checkbox; enable force: true since element is not visible/interactive  
+        expect(await box.isChecked()).toBeTruthy()                          //Assert that each checkbox is checked
+        await box.uncheck({ force: true })                                  //Uncheck each checkbox; enable force: true since element is not visible/interactive  
+        expect(await box.isChecked()).toBeFalsy()                          //Assert that each checkbox is unchecked 
+    }  
 
 })
